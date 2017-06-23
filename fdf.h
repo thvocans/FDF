033a97e5ecf7c9212f7058b0194d6a3f4330f971
Thib <thib@Thibs-iMac.local>
2017-06-23 03:36:53 +0200

    parsing en cours

--- fdf.h
+++ fdf.h
@@ -17,56 +17,61 @@
 
 # include "mlx.h"
 # include <stdio.h>
+# include <fcntl.h>
 # include "./libft/libft.h"
 
 typedef struct s_map t_map;
 struct s_map
 {
+	int		nb;//
 	char	*line;
-	int		*px;//0:height
-	int		*cl;//color
+	int		*px;//p_height
+	int		*pc;//p_color
 	t_map	*next;
 };
 
 struct s_img
 {
-	void	*pt;
-	int		*px;
-	int		ln;
-	int		bpp;
-	int		end;
+	void	*pt;		//struct img pointer new img
+
+//values returned by get_data_addr
+	int		*px;		//img string casted : int = px color
+						//bits : AARRGGBB white : 0xFFFFFF
+	int		ln;			//line len
+	int		bpp;		//bits per pixel
+	int		end;		//endian
 };
 typedef struct s_img t_img;
 
 struct s_key
 {
-	unsigned char	down[40];
-	short			oct;
-	short			bit;
-	short			mask;
-	unsigned int	*pt;
+	unsigned char	down[40];	//key pressed tab
+	short			oct;		//which oct to check
+	short			bit;		//which bit in oct
+	short			mask;		//check this bit only
+	unsigned int	*pt;		//pt to down tab
 };
 typedef struct s_key t_key;
 
 struct s_mlx
 {
-	void	*mlx;
-	void	*win;
-	int		x;
-	int		y;
+	void	*mlx;		//mlx server struct pointer
+	void	*win;		//1st window pointer
+	int		x;			//horizontal len / number of px in line
+	int		y;			//vertical len / number of lines
 
-	int		fd;
+	int		fd;			//open map filedescriptor
 
-	t_img	img;
-	t_key	key;
-	t_map	*map;
+	t_img	img;		//img related
+	t_key	key;		//key related stuff
+	t_map	*map;		//linked list with map infos
 };
 typedef struct s_mlx t_mlx;
 
-int     is_on(t_key *key);
-int     ft_key(int key, void *p);
-int     press(int key, void *p);
-int     release(int key, void *p);
-void    ft_init_key(t_key *key);
-int		parser(t_mlx *w, char *av);
+int     is_on(t_key *key);				//not used yet
+int     ft_key(int key, void *p);		//key pressed to img
+int     press(int key, void *p);		//toggle pressed keys
+int     release(int key, void *p);		//toggle released keys
+void    ft_init_key(t_key *key);		//init all to 0
+int		parser(t_mlx *w, char *av);		//reading stuff starts here
 # endif
--- parsing.c
+++ parsing.c
@@ -17,24 +17,53 @@ int		*get_nbr(t_map *map)
 	int		i;
 	int		j;
 	char	**split;
+	
+	i = 0;
+	j = 0;
+	split = ft_strsplit(map->line, ' ');
+	
+	while (split[i]) //longueur du tableau
+		i++;
+	map->nb = i;
+	//malloc du nombre d'ints
+	if (!(map->px = malloc(sizeof(int) * i)) ||
+			!(map->cl = malloc(sizeof(int) * i)))
+		return (NULL);
+	while (j < i) //heigh to num + color to num
+	{
+		map->px[j] = ft_atoi(split[j]);
+		if (ft_strchr(split[j], ','))
+/**/		map->pc[j] = ;//put here atoi base to convert hex to int
+		else
+			map->pc[j] = 0xFFFFFF;
+	}
 
-	split = ft_strsplit(line, ' ');
 
-	return (out);
+
+/*	while (split[i])
+	{
+		printf("split%s\n", split[i++]);
+	}
+*/
+	return (0);
 }
 
 int		parser(t_mlx *w, char *av)
 {
 	int		fd;
-	char	*line;
 
 	w->map = malloc(sizeof(*w->map));
 	fd = open(av, O_RDONLY);
-	gnl(fd, &line);
+	//read until line 3 to check if values are ok
+	gnl(fd, &w->map->line);
+	gnl(fd, &w->map->line);
+	gnl(fd, &w->map->line);
 	get_nbr(w->map);
 
 	/*	while (gnl(fd, &line) > 0)
 	{
 		
 	}
-*/}
+*/
+	return (0);
+}
